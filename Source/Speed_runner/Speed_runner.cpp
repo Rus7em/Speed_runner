9db119a2db6585a7de3b0c46dde6b2f5e1f179a5
Rus7em <rustem.kildibekov@gmail.com>
2018-06-13 20:10:44 +0300

    Initial commit

---
+++ Source/Speed_runner/Speed_runner.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Speed_runner.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Speed_runner, "Speed_runner" );
---
+++ Source/Speed_runner/Speed_runnerGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Speed_runnerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPEED_RUNNER_API ASpeed_runnerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
